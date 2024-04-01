d2e4e4ad08505fafe1f71e776d272824bac43b0b
Sergey White <sergey_white86@mail.ru>
2024-04-01 20:37:32 +0300

    init commit

---
+++ src/main/java/com/white/skyprocoursework2/SkyProCourseWork2Application.java
@@ -0,0 +1,13 @@
+package com.white.skyprocoursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProCourseWork2Application.class, args);
+	}
+
+}
